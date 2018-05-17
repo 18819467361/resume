7243d958dbd1a789225dc5a461a28270907bb07a
yjy <yejiayuan163@163.com>
2018-05-18 00:41:25 +0800

    个人简历第一天进度.2

--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../../node_modules/bootstrap/dist/js/bootstrap.min.js'
-// import '../../node_modules/bootstrap/js/dist/collapse.js'
-import '../css/index.css'
